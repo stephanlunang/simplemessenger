b4feb6908b4a9fe5b087fdb8caeadc00ad177b36
Stephan Ng <stephanng@Stephans-MacBook-Pro.local>
2016-01-21 14:53:11 -0800

    Goes to bottom when field is clicked, but doesnt go to bottom when pressing enter.

--- Documents/simplemessenger/simplemessenger.js
+++ Documents/simplemessenger/simplemessenger.js
@@ -13,12 +13,15 @@ if (Meteor.isClient) {
       		$scope.messages = $meteor.collection(Messages);
 
       		$scope.addMessage = function (newMessage) {
-        		$scope.messages.push( {
-          		text: newMessage,
-          		createdAt: new Date(), 
-          		username: Meteor.user().username
-          	}
-        );
-      };
+        		$scope.messages.push( 
+        			{text: newMessage, 
+        				createdAt: new Date(), 
+        				username: Meteor.user().username 
+        			});
+      		};
+      		$scope.gotoBottom = function (){
+    			var objDiv = document.getElementById("cardcontainer");
+       			objDiv.scrollTop = objDiv.scrollHeight;      			
+      		}
     }]);
 }
