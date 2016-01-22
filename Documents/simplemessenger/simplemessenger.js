34b523ec098e9ca524fc0d21b4c6b7042f61c9dd
Stephan Ng <stephanng@Stephans-MacBook-Pro.local>
2016-01-21 17:54:47 -0800

    Somewhat navbar integrated, but also fixed blank bug without username (now displays email.)

--- Documents/simplemessenger/simplemessenger.js
+++ Documents/simplemessenger/simplemessenger.js
@@ -1,11 +1,35 @@
 Messages = new Mongo.Collection('messages');
 
 if (Meteor.isClient) {
-	angular.module('simplemessenger',['angular-meteor', 'accounts.ui']);
+	angular.module('simplemessenger',[
+		'angular-meteor', 
+		'accounts.ui',
+		'ui.router'
+		]);
 
   	Accounts.ui.config({
     	passwordSignupFields: "USERNAME_AND_EMAIL"
   	});
+
+  	angular.module('simplemessenger').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+	    $locationProvider.html5Mode(true);
+
+	    $stateProvider
+	      .state('landing', {
+	        url: '/landing',
+	        templateUrl: 'landing.html'
+	      })
+	      .state('simplemessenger', {
+	        url: '/simplemessenger',
+	        templateUrl: 'simplemessenger.html'
+	      })
+	      .state('users', {
+	        url: '/users',
+	        templateUrl: 'users.html'
+	      });
+
+	    $urlRouterProvider.otherwise("/landing");
+	});  	
 
   	angular.module('simplemessenger').controller('MessengerCtrl', ['$scope', '$meteor',
     	function ($scope, $meteor) {
@@ -16,7 +40,8 @@ if (Meteor.isClient) {
         		$scope.messages.push( 
         			{text: newMessage, 
         				createdAt: new Date(), 
-        				username: Meteor.user().username 
+        				username: Meteor.user().username, 
+        				email: Meteor.user().emails[0].address
         			});
       		};
       		$scope.gotoBottom = function (){
@@ -24,4 +49,18 @@ if (Meteor.isClient) {
        			objDiv.scrollTop = objDiv.scrollHeight;      			
       		}
     }]);
+
+	angular.module('simplemessenger').filter('displayName', function () {
+	  return function (message) {
+	    if (!message) {
+	      return '';
+	    } 
+	    if (message.username) {
+	      return message.username;
+	    }
+	    else {
+	      return message.email;
+	    }
+	  }
+	});
 }
